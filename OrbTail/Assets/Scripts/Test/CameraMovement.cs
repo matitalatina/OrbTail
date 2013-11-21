f77ef527ed34eaf1ef1f236a70dcf1ef7dd84d7c
Mattia Natali <matitalatina@gmail.com>
2013-11-21 23:13:46 +0100

    Add ShipTest scene to tweak handling parameters of the ship. Add Materials asset.

---
+++ OrbTail/Assets/Scripts/Test/CameraMovement.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	public float smooth = 1.5f;         // The relative speed at which the camera will catch up.
+	public float relDistancePos = -5f;
+	public float relHighPos = 3f;
+	
+	private Transform player;           // Reference to the player's transform.
+	private Vector3 relCameraPos;       // The relative position of the camera from the player.
+	private float relCameraPosMag;      // The distance of the camera from the player.
+	private Vector3 newPos;             // The position the camera is trying to reach.
+
+
+	
+	
+	void Awake ()
+	{
+		// Setting up the reference.
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+		
+		// Setting the relative position as the initial relative position of the camera in the scene.
+		relCameraPos = transform.position - player.position;
+		relCameraPosMag = relCameraPos.magnitude - 0.5f;
+	}
+	
+	
+	void FixedUpdate ()
+	{
+		// The standard position of the camera is the relative position of the camera from the player.
+		//Vector3 standardPos = player.position + relCameraPos;
+		Vector3 standardPos = player.position + player.forward * relDistancePos + relHighPos * Vector3.up;
+		
+		newPos = standardPos;
+		// Lerp the camera's position between it's current position and it's new position.
+		transform.position = Vector3.Lerp(transform.position, newPos, smooth * Time.deltaTime);
+		
+		// Make sure the camera is looking at the player.
+		SmoothLookAt();
+	}
+	
+	
+	bool ViewingPosCheck (Vector3 checkPos)
+	{
+		RaycastHit hit;
+		
+		// If a raycast from the check position to the player hits something...
+		if(Physics.Raycast(checkPos, player.position - checkPos, out hit, relCameraPosMag))
+			// ... if it is not the player...
+			if(hit.transform != player)
+				// This position isn't appropriate.
+				return false;
+		
+		// If we haven't hit anything or we've hit the player, this is an appropriate position.
+		newPos = checkPos;
+		return true;
+	}
+	
+	
+	void SmoothLookAt ()
+	{
+		// Create a vector from the camera towards the player.
+		Vector3 relPlayerPosition = player.position - transform.position;
+		
+		// Create a rotation based on the relative position of the player being the forward vector.
+		Quaternion lookAtRotation = Quaternion.LookRotation(relPlayerPosition, Vector3.up);
+		
+		// Lerp the camera's rotation between it's current rotation and the rotation that looks at the player.
+		transform.rotation = Quaternion.Lerp(transform.rotation, lookAtRotation, smooth * Time.deltaTime);
+	}
+}
---
+++ OrbTail/Assets/Scripts/Test/MovementControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementControllerTest : MonoBehaviour {
+	public float upForce = 1f;
+	public float torqueForce = 1f;
+	public float speedForce = 1f;
+	public float maxRoll = 30f;
+	public float rollSmooth = 30f;
+	public float hoverForce = 50f;
+	public float hoverDistance = 5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//this.rigidbody.AddTorque(Vector3.up * Input.GetAxis("Horizontal") * torqueForce);
+		this.rigidbody.AddRelativeForce(Vector3.forward * Input.GetAxis("Vertical") * speedForce);
+		this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.Euler(0, this.transform.rotation.eulerAngles.y + Input.GetAxis("Horizontal") * torqueForce, -Input.GetAxis("Horizontal") * maxRoll), rollSmooth * Time.deltaTime);
+
+		if(Physics.Raycast(transform.position, Vector3.down, hoverDistance)) {
+			float realHoverForce = hoverForce * 2;
+			rigidbody.AddForce(Vector3.up * realHoverForce);
+		} else {
+			rigidbody.AddForce(Vector3.down * hoverForce / 2);
+		}
+
+	}
+
+}
