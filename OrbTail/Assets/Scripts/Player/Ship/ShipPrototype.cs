b14c29bcdf57ec2dc91d71a51b8b400efd314757
Raffaele Daniele Facendola <raf.dan.fac@gmail.com>
2013-11-26 22:42:37 +0100

    ShipPrototype has been implemented

---
+++ OrbTail/Assets/Scripts/Player/Ship/ShipPrototype.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipPrototype : MonoBehaviour {
+
+    /// <summary>
+    /// The defence power of the ship (in the range 1..5)
+    /// </summary>
+    public int defence;
+
+    /// <summary>
+    /// The offence power of the ship (in the range 1..5)
+    /// </summary>
+    public int offence;
+
+    /// <summary>
+    /// The steering of the ship (in the range 1..5)
+    /// </summary>
+    public int steering;
+
+    /// <summary>
+    /// The speed of the ship (in the range 1..5)
+    /// </summary>
+    public int speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Awake(){
+
+        MovementController movement_controller = GetComponent<MovementController>();
+
+        movement_controller.GetEngineDriverStack().Push( new DefaultEngineDriver(speed) );
+        movement_controller.GetWheelDriverStack().Push( new DefaultWheelDriver(steering) );
+
+        TailController tail_controller = GetComponent<TailController>();
+
+        tail_controller.GetOffenceDriverStack().Push( new DefaultOffenceDriver(offence) );
+        tail_controller.GetDefenceDriverStack().Push( new DefaultDefenceDriver(defence) );
+        tail_controller.GetAttacherDriverStack().Push( new DefaultAttacherDriver() );
+    
+        
+    
+    }
+
+}
