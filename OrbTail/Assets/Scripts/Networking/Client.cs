82cdef487a0bd7f9f2c3071cda9c8d4358e50f98
Raffaele Daniele Facendola <raf.dan.fac@gmail.com>
2013-11-29 20:53:51 +0100

    Client`server test architecture has been partially implemented

---
+++ OrbTail/Assets/Scripts/Networking/Client.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Client : MonoBehaviour {
+
+    void Awake()
+    {
+
+        MasterServer.RequestHostList("OrbTail");
+
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+        
+
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMasterServerEvent(MasterServerEvent msEvent)
+    {
+        if (msEvent == MasterServerEvent.HostListReceived)
+        {
+           
+            HostData[] data = MasterServer.PollHostList();
+
+            if (data.Length > 0)
+            {
+
+                Network.Connect(data[0]);
+
+                Debug.Log("Connecting to a server...");
+
+            }
+
+        }
+
+    }
+
+
+    void OnConnectedToServer()
+    {
+
+        Debug.Log("Connected to server");
+
+    }
+
+        
+}
