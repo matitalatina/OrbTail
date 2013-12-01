a73756138abc3f3fa1b51939c7d5ac6b43385133
Mattia Natali <matitalatina@gmail.com>
2013-12-01 21:22:39 +0100

    AiPlayer improved

--- OrbTail/Assets/Scripts/AI/PlayerAI.cs
+++ OrbTail/Assets/Scripts/AI/PlayerAI.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class PlayerAI : MonoBehaviour {
 
 	private RelayInputBroker inputBroker = new RelayInputBroker();
+	private GameObject target;
+	private Vector3 desideredDirection;
+	private bool alreadyCollided;
 
 	public IInputBroker GetInputBroker() {
 		return inputBroker;
@@ -17,17 +20,60 @@ public class PlayerAI : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		inputBroker.Acceleration = 1f;
-		inputBroker.Steering = 1f;
+		if (target != null) {
+			if (target.tag == Tags.Ship) {
+				if (!alreadyCollided) {
+					Chasing();
+				}
+				else {
+					GoAway();
+				}
+			}
+			else {
+				Chasing();
+			}
+		}
+		else {
+			LookAround();
+		}
+
+		AvoidOstacles();
+
+		inputBroker.Acceleration(desideredDirection.z);
+		inputBroker.Steering(desideredDirection.x);
 	}
 
 	void Chasing() {
-
+		Vector3 relVector = target.transform.position - gameObject.transform.position;
+		desideredDirection = relVector.normalized;
 	}
 
 	void GoAway() {
+		Vector3 relVector = gameObject.transform.position - target.transform.position;
+		desideredDirection = relVector.normalized;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		GameObject colObject = other.gameObject;
+
+		if (target == null) {
+			if (colObject.tag == Tags.Ship || IsFreeOrb(colObject)) {
+				target = colObject;
+			}
+		}
+	}
+
+	void AvoidOstacles() {
 
 	}
-	
+
+	void LookAround() {
+
+	}
+
+
+	private bool IsFreeOrb(GameObject orb) {
+		return orb.tag == Tags.Orb && !orb.GetComponent<OrbController>().IsAttached();
+	}
 
 }
