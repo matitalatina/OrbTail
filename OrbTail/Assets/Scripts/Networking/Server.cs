f54448bf0bbe069a9ca72af812ea166e54586f6c
Raffaele Daniele Facendola <raf.dan.fac@gmail.com>
2013-12-01 21:42:34 +0100

    Coherency commit

--- OrbTail/Assets/Scripts/Networking/Server.cs
+++ OrbTail/Assets/Scripts/Networking/Server.cs
@@ -43,7 +43,7 @@ public class Server : MonoBehaviour {
         Debug.Log("A player has been connected, let the game begin!");
 
         //Instantiate a normal orb
-        var orb = Resources.Load("Prefabs/Orb");
+        var orb = Resources.Load("Prefabs/Ship");
 
         Network.Instantiate(orb, Vector3.zero, Quaternion.identity, 0);
 
