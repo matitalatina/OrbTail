7f2dcb856383ea9436200cc332c56431d81a311d
Mattia Natali <matitalatina@gmail.com>
2013-12-15 13:16:52 +0100

    Dimension of the bubble reduced

--- OrbTail/Assets/Scripts/Power/Effects/Bubble.cs
+++ OrbTail/Assets/Scripts/Power/Effects/Bubble.cs
@@ -11,6 +11,6 @@ public class Bubble : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         float bubblingFactor = Mathf.Cos(Time.time * 2.0f);
-        this.transform.localScale = Vector3.one * (5 + bubblingFactor * 0.5f);
+        this.transform.localScale = Vector3.one * (2 + bubblingFactor * 0.5f);
 	}
 }
