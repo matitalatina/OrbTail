0bc7f7adc6d9df73c5287ae506f365d7f7e10981
Mattia Natali <matitalatina@gmail.com>
2013-12-16 23:01:41 +0100

    Now ship have proximity field to gather orbs

---
+++ OrbTail/Assets/Scripts/Player/Drivers/Controllers/ProximityHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProximityHandler : MonoBehaviour {
+	TailController tailController;
+
+	// Use this for initialization
+	void Start () {
+		tailController = transform.parent.gameObject.GetComponent<TailController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		tailController.OnProximityEnter(other);
+	}
+}
