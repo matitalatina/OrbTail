46587a9284a1cdda12cd906ec1cac18c75365413
Mattia Natali <matitalatina@gmail.com>
2013-12-29 20:51:26 +0100

    Added HUD indicators of ships

--- OrbTail/Assets/HUDOpponentsPosition.cs
+++ OrbTail/Assets/HUDOpponentsPosition.cs
@@ -3,42 +3,70 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class HUDOpponentsPosition : MonoBehaviour {
-	private const string HUDIndicator = "•";
+	private const string HUDIndicatorPrefabPath = "Prefabs/HUD/ShipIndicator";
+	private const float boostAlpha = 0.9f;
+	private const float distanceFromShip = 3f;
 	private Transform myShipPosition;
-	private Dictionary<Transform, GameObject> positionsAndIndicators;
+	private bool gameStarted = false;
+	private Dictionary<Transform, GameObject> positionShipAndIndicators;
+	private Dictionary<GameObject, TextMesh> textMeshes;
 
 	// Use this for initialization
 	void Start () {
 		GameBuilder builder = GameObject.FindGameObjectWithTag(Tags.Master).GetComponent<GameBuilder>();
 		builder.EventGameBuilt += OnGameBuilt;
+		positionShipAndIndicators = new Dictionary<Transform, GameObject>();
+		textMeshes = new Dictionary<GameObject, TextMesh>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (gameStarted) {
+			foreach (KeyValuePair<Transform, GameObject> pair in positionShipAndIndicators) {
+				GameObject indicator = pair.Value;
+				Vector3 directionToOpponent = (pair.Key.position - myShipPosition.position).normalized;
+				indicator.transform.localPosition = myShipPosition.position + directionToOpponent * distanceFromShip;
+				indicator.transform.rotation = myShipPosition.rotation;
+
+				float alpha = Mathf.Clamp(-Vector3.Dot(myShipPosition.forward, directionToOpponent) + boostAlpha, 0, 1);
+				Color newColor = textMeshes[indicator].color;
+				newColor.a = alpha;
+				textMeshes[indicator].color = newColor;
+			}
+		}
 	}
 
 	private void OnGameBuilt(object sender) {
 		Game game = GameObject.FindGameObjectWithTag(Tags.Game).GetComponent<Game>();
 		game.EventEnd += OnEventEnd;
+		game.EventStart += OnEventStart;
 		GameObject myShip = game.ActivePlayer;
 		myShipPosition = myShip.transform;
 
 		foreach (GameObject ship in game.ShipsInGame) {
 			if (ship != myShip) {
-				prepareHUDIndicator(ship);
+				addHUDIndicator(ship);
 			}
 		}
 	}
+	
 
 
-	private void prepareHUDIndicator(GameObject ship) {
+	private void addHUDIndicator(GameObject ship) {
+		GameObject indicator = (GameObject) Instantiate(Resources.Load(HUDIndicatorPrefabPath));
+		TextMesh textMeshIndicator = indicator.GetComponent<TextMesh>();
+		textMeshIndicator.color = ship.GetComponent<GameIdentity>().Color;
+		textMeshes.Add(indicator, textMeshIndicator);
+		positionShipAndIndicators.Add(ship.transform, indicator);
+	}
 
+	private void OnEventStart(object sender, int countdown) {
+		if (countdown <= 0) {
+			gameStarted = true;
+		}
 	}
 
 	private void OnEventEnd(object sender, GameObject winner, int info) {
-		
-
-		
+		gameStarted = false;
 	}
 }
