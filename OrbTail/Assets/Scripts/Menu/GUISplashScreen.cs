7ee29d5b2e92b351d20bda8a4789ee492cc73a83
Mattia Natali <matitalatina@gmail.com>
2014-01-30 17:48:52 +0100

    Added splash screen

---
+++ OrbTail/Assets/Scripts/Menu/GUISplashScreen.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUISplashScreen : MonoBehaviour {
+	private float timeOfSplash = 1.5f;
+	private float fadeTime = 0.3f;
+	private GameObject radiance;
+	private GameObject polimi;
+	private GameObject menteZero;
+
+	// Use this for initialization
+	void Start () {
+		radiance = GameObject.Find("LogoRadiance");
+		polimi = GameObject.Find("PolimiGameCollective");
+		menteZero = GameObject.Find("MenteZero");
+
+		iTween.FadeTo(polimi, 0f, 0f);
+		iTween.FadeTo(menteZero, 0f, 0f);
+		StartCoroutine("changeSecondSplash");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private IEnumerator changeSecondSplash() {
+		yield return new WaitForSeconds(timeOfSplash);
+		iTween.FadeTo(radiance, 0f, fadeTime);
+		iTween.FadeTo(polimi, 1f, fadeTime);
+		iTween.FadeTo(menteZero, 1f, fadeTime);
+		yield return new WaitForSeconds(timeOfSplash + fadeTime);
+		Application.LoadLevel("MenuMain");
+
+	}
+	
+}
